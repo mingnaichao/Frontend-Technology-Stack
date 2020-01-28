79a827928c428fdb6cf148f5cf613274eff93309
明乃超 <mingnaichao@qq.com>
2020-01-28 17:17:11 +0800

    优化代码

--- TypeScript/20-方法装饰器、方法参数装饰器、装饰器的执行顺序/index.ts
+++ TypeScript/20-方法装饰器、方法参数装饰器、装饰器的执行顺序/index.ts
@@ -113,74 +113,73 @@
 //属性》方法》方法参数》类
 // 如果有多个同样的装饰器，它会先执行后面的
 
-
-function logClass1(params: string) {
-    return function (target: any) {
-        console.log('类装饰器1')
-    }
-}
-
-function logClass2(params: string) {
-    return function (target: any) {
-        console.log('类装饰器2')
-    }
-}
-
-function logAttribute1(params?: string) {
-    return function (target: any, attrName: any) {
-        console.log('属性装饰器1')
-    }
-}
-
-function logAttribute2(params?: string) {
-    return function (target: any, attrName: any) {
-        console.log('属性装饰器2')
-    }
-}
-
-function logMethod1(params?: string) {
-    return function (target: any, attrName: any, desc: any) {
-        console.log('方法装饰器1')
-    }
-}
-function logMethod2(params?: string) {
-    return function (target: any, attrName: any, desc: any) {
-        console.log('方法装饰器2')
-    }
-}
-
-function logParams1(params?: string) {
-    return function (target: any, attrName: any, desc: any) {
-        console.log('方法参数装饰器1')
-    }
-}
-
-function logParams2(params?: string) {
-    return function (target: any, attrName: any, desc: any) {
-        console.log('方法参数装饰器2')
-    }
-}
-
-
-
-@logClass1('http://www.itying.com/api')
-@logClass2('xxxx')
-class HttpClient {
-    @logAttribute1()
-    @logAttribute2()
-    public apiUrl: string | undefined;
-    constructor() {
+/*
+    function logClass1(params: string) {
+        return function (target: any) {
+            console.log('类装饰器1')
+        }
     }
 
-    @logMethod1()
-    @logMethod2()
-    getData() {
-        return true;
+    function logClass2(params: string) {
+        return function (target: any) {
+            console.log('类装饰器2')
+        }
     }
 
-    setData(@logParams1() attr1: any, @logParams2() attr2: any, ) {
-
+    function logAttribute1(params?: string) {
+        return function (target: any, attrName: any) {
+            console.log('属性装饰器1')
+        }
     }
-}
 
-var http: any = new HttpClient();
+    function logAttribute2(params?: string) {
+        return function (target: any, attrName: any) {
+            console.log('属性装饰器2')
+        }
+    }
+
+    function logMethod1(params?: string) {
+        return function (target: any, attrName: any, desc: any) {
+            console.log('方法装饰器1')
+        }
+    }
+    function logMethod2(params?: string) {
+        return function (target: any, attrName: any, desc: any) {
+            console.log('方法装饰器2')
+        }
+    }
+
+    function logParams1(params?: string) {
+        return function (target: any, attrName: any, desc: any) {
+            console.log('方法参数装饰器1')
+        }
+    }
+
+    function logParams2(params?: string) {
+        return function (target: any, attrName: any, desc: any) {
+            console.log('方法参数装饰器2')
+        }
+    }
+
+    @logClass1('http://www.itying.com/api')
+    @logClass2('xxxx')
+    class HttpClient {
+        @logAttribute1()
+        @logAttribute2()
+        public apiUrl: string | undefined;
+        constructor() {
+        }
+
+        @logMethod1()
+        @logMethod2()
+        getData() {
+            return true;
+        }
+
+        setData(@logParams1() attr1: any, @logParams2() attr2: any, ) {
+
+        }
+    }
+
+    var http: any = new HttpClient();
+*/
