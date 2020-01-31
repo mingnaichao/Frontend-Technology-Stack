9012b04dd0c26feed3be1713b6209c4d9b1feeef
明乃超 <mingnaichao@qq.com>
2020-01-31 20:25:30 +0800

    todoList

--- Angular8/06-实现一个完整的toDoList、以及类似京东App搜索缓存数据功能/angulardemo06/src/app/components/todo-list/todo-list.component.ts
+++ Angular8/06-实现一个完整的toDoList、以及类似京东App搜索缓存数据功能/angulardemo06/src/app/components/todo-list/todo-list.component.ts
@@ -7,6 +7,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoListComponent implements OnInit {
 
+  public keyWord: string;
+  public todoList: any[] = [];
+
+  // 添加数据
+  toAdd(e: any): void {
+    if (e.keyCode === 13) {
+      if (!this.todolistHasKeyword(this.todoList, this.keyWord)) {
+        this.todoList.push({
+          title: this.keyWord,
+          status: 0 // 0表示代办事项  1表示已完成事项
+        });
+        this.keyWord = '';
+      } else {
+        alert('数据已经存在');
+        this.keyWord = '';
+      }
+    }
+  }
+
+  // 校验数据是否存在
+  todolistHasKeyword(todoList: any, keyWord: string): boolean {
+    if (!keyWord) { return false; }
+    for (const item of todoList) {
+      if (item.title === keyWord) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  // 删除数据
+  deleteWord(i: number): void {
+    this.todoList.splice(i, 1);
+  }
+
   constructor() { }
 
   ngOnInit() {
