049e55c2fc593d9b5ff73cf75e9e6f94a60cb390
明乃超 <mingnaichao@qq.com>
2020-02-01 13:22:10 +0800

    Dom操作以及@ViewChild

---
+++ Angular8/08-Dom操作以及@ViewChild/angulardemo08/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NewsComponent } from './components/news/news.component';
+import { HeaderComponent } from './components/header/header.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NewsComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Angular8/08-Dom操作以及@ViewChild/angulardemo08/src/app/components/news/news.component.ts
@@ -0,0 +1,53 @@
+/*
+  ViewChild获取dom节点
+    1、模板中给dom起一个名字
+      <div #myBox>
+        我是一个dom节点
+      </div>
+
+    2、在业务逻辑里面引入ViewChild
+    import { Component, OnInit,ViewChild} from '@angular/core';
+
+    3、写在类里面
+      @ViewChild('myBox') myBox:any;
+
+    4、ngAfterViewInit生命周期函数里面获取dom
+      this.myBox.nativeElement
+*/
+
+import { Component, OnInit, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-news',
+  templateUrl: './news.component.html',
+  styleUrls: ['./news.component.scss']
+})
+export class NewsComponent implements OnInit {
+
+  // 获取dom节点
+  @ViewChild('myBox', { static: false }) myBox: any;
+  // 获取子组件
+  @ViewChild('header', { static: false }) header: any;
+
+  constructor() { }
+
+  // 初始化指令/组件
+  ngOnInit() {
+  }
+
+  // 初始化完组件视图及其子视图之后调用
+  ngAfterViewInit(): void {
+    console.log('1', this.myBox);
+    console.log('2', this.myBox.nativeElement);
+    this.myBox.nativeElement.style.width = '100px';
+    this.myBox.nativeElement.style.height = '100px';
+    this.myBox.nativeElement.style.background = 'red';
+    console.log('3', this.myBox.nativeElement.innerHTML);
+  }
+
+  getChildFunc(): void {
+    console.log(this.header); // 获取子组件类
+    this.header.run(); // 执行子组件run方法
+  }
+
+}
